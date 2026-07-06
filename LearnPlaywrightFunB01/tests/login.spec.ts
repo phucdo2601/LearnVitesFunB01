892f76bfaa79378c988cc46dcdd2b0d4b5c27f26
phucdn <phucdn@allexceed.co.jp>
2026-07-06 15:20:01 +0700

    [learn-playwright-fun-b01] 04. Basic learn How To Maximize Browser Window In playwright  TestCase b01

--- LearnPlaywrightFunB01/tests/login.spec.ts
+++ LearnPlaywrightFunB01/tests/login.spec.ts
@@ -1,7 +1,18 @@
 import { test, expect } from '@playwright/test';
 
+test.use({
+    viewport: {
+        width: 1500,
+        height: 1000
+    }
+})
+
 test("Valid login", async function({page}) {
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+    console.log(await page.viewportSize()?.width);
+    console.log(await page.viewportSize()?.height);
+    
 
     await page.getByPlaceholder("Username").type("Admin")
 
